7ba1470e870ddb6768649186c7ca046418db7ee7
non-dev777 <nobu.ken.777@gmail.com>
2025-01-29 10:41:08 +0900

    ✨ Add Footer component with basic structure

---
+++ app/components/Footer.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/outline";
+
+const Footer = () => {
+  return <footer className="bg-gray-100 py-4"></footer>;
+};
+
+export { Footer };
