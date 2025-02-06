57e9468498b7e24702af2a641f5feb6086e15e2d
non-dev777 <nobu.ken.777@gmail.com>
2025-02-06 11:13:01 +0900

    ✨ Enhance Strength component with detailed service offerings and pricing

--- app/components/Strength.tsx
+++ app/components/Strength.tsx
@@ -3,8 +3,20 @@
 import { motion } from "framer-motion";
 import { ShieldCheckIcon, RocketLaunchIcon } from "@heroicons/react/24/outline";
 
+type CanDos = {
+  [key: string]: string[];
+};
+
+interface Strength {
+  icon: React.ElementType;
+  title: string;
+  description: string;
+  canDos: CanDos;
+  pricing: string;
+}
+
 const Strength = () => {
-  const strengths = [
+  const strengths: Strength[] = [
     {
       icon: ShieldCheckIcon,
       title: "セキュアでSEOに強いWebサイト制作",
@@ -12,6 +24,20 @@ const Strength = () => {
         "情報安全確保支援士の資格を活かしたセキュアなWebサイト制作と、SEOブログ運営経験を活かしたSEO対策を提供します。" +
         "\n\n" +
         "実績としては個人で活動したYouTubeの登録者は8700人を超え、ブログも月間8万円の収益なども達成しています。",
+      canDos: {
+        Webサイト制作: [
+          "WordPressのテーマ開発。独自のデザインで記事を投稿できるようなWebサイトを作成します。",
+          "独自デザインのWebサイトの作成。WordPressを使用しないサイトも作成します。",
+          "LPの作成。コンバージョンを意識したデザインでLPを作成します。",
+        ],
+        その他: [
+          "Webサイトのトラブルシューティング。WordPressなどWebサイトで問題が起きている場合は、原因を特定して解決します。",
+          "SEO対策のアドバイス。SEO対策を意識したWebサイトに修正します。",
+          "ブログの運営。SEO対策を意識したブログを運営します。",
+          "その他Webサイトに関することでお困りごとがありましたら、お気軽にお問い合わせください。",
+        ],
+      },
+      pricing: "無料でお見積りしております。お気軽にお問い合わせください。",
     },
     {
       icon: RocketLaunchIcon,
@@ -20,6 +46,19 @@ const Strength = () => {
         "「これを自動化できたらいいな」「こんなシステムがあったらいいな」という課題に対して、最適なソリューションを提供します。" +
         "\n\n" +
         "機能面はもちろん使いやすさを意識したデザインでシステムを提供します。",
+      canDos: {
+        Webシステム開発: [
+          "会員管理システムなどお客様のニーズに合わせたシステムを作成します。",
+        ],
+        自動化ツール: [
+          "日々のタスクのルーティン作業を自動化します。",
+          "VBAやGASも経験があるので、ExcelやGoogle Sheetsのシステムを作成します。",
+        ],
+        その他: [
+          "その他業務効率化に関することでお困りごとがありましたら、お気軽にお問い合わせください。",
+        ],
+      },
+      pricing: "無料でお見積りしております。お気軽にお問い合わせください。",
     },
   ];
 
@@ -71,7 +110,7 @@ const Strength = () => {
             >
               <div className="flex items-center mb-6">
                 <strength.icon className="h-12 w-12 text-cyan-500 mr-4" />
-                <h3 className="text-xl font-semibold text-gray-800">
+                <h3 className="text-xl font-semibold text-gray-800 border-b border-cyan-500 pb-2">
                   {strength.title}
                 </h3>
               </div>
@@ -79,6 +118,33 @@ const Strength = () => {
               <p className="text-gray-600 mb-6 leading-relaxed">
                 {strength.description}
               </p>
+
+              <div className="mb-6">
+                <h4 className="text-lg font-semibold text-gray-800 mb-3 border-b border-gray-200 pb-2">
+                  できること
+                </h4>
+                {Object.entries(strength.canDos).map(([category, techs]) => (
+                  <div key={category} className="mb-4">
+                    <h5 className="text-md font-bold text-cyan-500 mb-2">
+                      {category}
+                    </h5>
+                    <ul className="list-disc pl-5">
+                      {techs.map((tech) => (
+                        <li
+                          className="text-gray-700 font-medium pb-2"
+                          key={tech}
+                        >
+                          {tech}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+
+              <div className="flex items-center">
+                <p className="text-gray-800 font-bold"> {strength.pricing}</p>
+              </div>
             </motion.div>
           ))}
         </motion.div>
